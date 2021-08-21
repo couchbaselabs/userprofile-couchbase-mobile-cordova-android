174a1be05de556e095b3c83671a4bb4c6a785c8f
biswas123 <biswas.khayargoli@gmail.com>
2021-08-21 22:59:26 +0545

    new logic to copy university database

--- query/src/app/login/login.component.ts
+++ query/src/app/login/login.component.ts
@@ -22,75 +22,74 @@ export class LoginComponent {
   constructor(private router: Router, private sharedService: SharedService, private file: File, private zip: Zip, private platform: Platform) { }
 
   ionViewDidEnter() {
-    this.email =  null;
-    this.password = null; 
+    this.email = null;
+    this.password = null;
   }
 
   ngOnInit() {
 
-    let zipfileName = this.externalDBName + ".zip";
+    this.platform.ready().then(() => {
+      let zipfileName = this.externalDBName + ".zip";
+      let config = new CBL.DatabaseConfiguration(this.externalDBName, { directory: 'couchbase', encryptionKey: '' });
 
-    this.platform.ready().then(() => { 
-      
-      let databaseDir = "couchbase/" + this.externalDBName + ".cblite2";
-      this.file.checkDir(this.file.dataDirectory, databaseDir).then(rs => {
-
+      CBL.databaseExists(config, result=> {
+        if (result) {
           this.initializeUniversityDB();
+        } else {
 
-      }).catch(err => {
-       
-        // path to place database zip file = src/assets/resource/
-        // ionic automatically copies our asset to www directory
+          // path to place database zip file = src/assets/resource/
+          // ionic automatically copies our asset to www directory
+          let sourceDir = this.file.applicationDirectory + "www/assets/resource/";
+          let targetDir = this.file.externalCacheDirectory;
 
-        let sourceDir = this.file.applicationDirectory + "www/assets/resource/";
-        let targetDir = this.file.externalCacheDirectory;
+          // copy zip file to cache directory from our asset directory then unzip on data directory.
+          this.file.copyFile(sourceDir, zipfileName, targetDir, zipfileName).then(rs => {
+            this.zip.unzip(rs.nativeURL, this.file.dataDirectory).then((resultCode) => {
 
-        // copy zip file to cache directory from our asset directory then unzip on data directory.
-        this.file.copyFile(sourceDir, zipfileName, targetDir, zipfileName).then(rs => {
-          this.zip.unzip(rs.nativeURL, this.file.dataDirectory).then((resultCode) => {
-            if (resultCode != -1) {
+              if (resultCode != -1) {
 
-              let currentConfig = { 'encryptionKey': '', 'directory' : ''}
-              let newConfig = { 'encryptionKey': '', 'directory' : 'couchbase'}
-
-            
-              CBL.copyDatabase(this.externalDBName, currentConfig, this.externalDBName, newConfig, (result: any) => {
+                let newConfig = new CBL.DatabaseConfiguration(this.externalDBName, { directory: 'couchbase', encryptionKey: '' });
+                CBL.copyDatabase(this.externalDBName, newConfig, (result: any) => {
                   this.createUniversityDatabaseIndexes();
-              }, (err: any) => {
-                console.log(err);
-              });
+                }, (err: any) => {
+                  console.log(err);
+                });
 
-            } else {
-              console.error("Could not unzip file.");
-            }
+              } else {
+                console.error("Could not unzip file.");
+              }
+
+            }).catch(err => {
+              console.error(err);
+            });
           }).catch(err => {
             console.error(err);
           });
-        }).catch(err => {
-          console.error(err);
-        });
-
+        }
+      }, err => {
+        console.error(err);
       });
     });
-
   }
+
+
   initializeUniversityDB() {
 
-    let config = new CBL.DatabaseConfiguration(this.externalDBName, { directory: 'couchbase', encryptionKey: ''});
+    let config = new CBL.DatabaseConfiguration(this.externalDBName, { directory: 'couchbase', encryptionKey: '' });
     CBL.createOrOpenDatabase(config, (result: any) => {
       console.log('University database Initialized : ' + result);
     }, (err: any) => {
       console.log(err);
     });
   }
-  
+
   createUniversityDatabaseIndexes() {
 
     let dbName = "universities";
     let indexName = "nameLocationIndex";
-    let indexes = ['name','location'];
+    let indexExpressions = ['name', 'location'];
 
-    CBL.createValueIndex(dbName, indexName, indexes, (result: any) => {
+    CBL.createValueIndex(dbName, indexName, indexExpressions, (result: any) => {
       console.log('University database Index created: ' + result);
     }, (err: any) => {
       console.log(err);
@@ -99,7 +98,7 @@ export class LoginComponent {
 
   onSubmit() {
 
-    let config = new CBL.DatabaseConfiguration(this.email, { directory: 'couchbase', encryptionKey: ''});
+    let config = new CBL.DatabaseConfiguration(this.email, { directory: 'couchbase', encryptionKey: '' });
     CBL.createOrOpenDatabase(config, (result: any) => {
       console.log('Database Initialized : ' + result);
 
@@ -110,7 +109,7 @@ export class LoginComponent {
       console.log(err);
     });
   }
-  
+
 
 
 
